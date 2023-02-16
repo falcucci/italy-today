d0322a39c82f9ef1522b131339faf77d0d5cadc3
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-02-16 21:03:12 +0100

    adds the `@vercel/analytics` package to the project and configures it to be used in `_app.js`.
    the following changes were made:
    - added `@vercel/analytics` to `package-lock.json` and `package.json`
    - added the `analytics` component to `_app.js`

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,18 @@
-// 1. import `NextUIProvider` component
+import { Analytics } from "@vercel/analytics/react";
 import { createTheme, NextUIProvider } from "@nextui-org/react";
 
-// 2. Call `createTheme` and pass your custom theme values
 const theme = createTheme({
   type: "dark", // it could be "light" or "dark"
 });
 
 function App({ Component, pageProps }) {
   return (
-    // 2. Use at the root of your app
-    <NextUIProvider theme={theme}>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      <NextUIProvider theme={theme}>
+        <Component {...pageProps} />
+      </NextUIProvider>
+      <Analytics />
+    </>
   );
 }
 
