ec872193185242965cf4ed57aa24ae6ee2d14a58
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-02-17 23:07:41 +0100

    add promisehandler function in utils/index.js
    adds a new function called `promisehandler` to the `utils/index.js` file. this function takes a promise and returns either the data on success or an error on failure. the function returns an array with the first element being either an error or `null` and the second element being either the data or the error.
    
    adds the following code to `utils/index.js`:
    
    ```js
    /**
     *
     * takes a promise and returns the promise's data on success or
     * an error on failure.
     *
     * @param {promise} promise - the promise to be handled
     *
     * @returns {array} - an array with the first element being either
     * an error or null and the second element being either the data
     * or the error.
     */
    const promisehandler = promise => {
      return promise.then(data => [null, data]).catch(err => [err]);
    };
    ```

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,18 @@
+/**
+ *
+ * takes a promise and returns the promise's data on success or
+ * an error on failure.
+ *
+ * @param {Promise} promise - The promise to be handled
+ *
+ * @returns {Array} - An array with the first element being either
+ * an error or null and the second element being either the data
+ * or the error.
+ */
 const promiseHandler = promise => {
   return promise.then(data => [null, data]).catch(err => [err]);
-}
+};
 
 module.exports = {
-  promiseHandler
-}
+  promiseHandler,
+};
